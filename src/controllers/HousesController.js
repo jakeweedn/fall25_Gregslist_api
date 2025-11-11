8f88b923b3850c4cf18d725244b83ab8c7070710
Jake <weednj@gmail.com>
2025-11-11 11:48:00 -0700

    Queries and Filters

--- src/controllers/HousesController.js
+++ src/controllers/HousesController.js
@@ -1,3 +1,4 @@
+import e from "express";
 import { housesService } from "../services/HousesService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -7,8 +8,38 @@ export class HousesController extends BaseController {
     constructor() {
 
         super('api/houses')
-        this.router.get('', this.getAllHouses)
-        this.router.get('/:houseId', this.getHouseById)
+        this.router
+            .get('', this.getAllHouses)
+            .get('/search', this.searchHouses)
+            .get('/:houseId', this.getHouseById)
+
+        //The order of the get requests matters! Put the id last!
+        //Put the question mark after search. In the lab reference, the endpoint is probably ''.
+        // http://localhost:3000/api/houses/search?levels=2
+
+
+    }
+
+
+    async searchHouses(request, response, next) {
+
+        try {
+            const searchQuery = request.query
+            const houses = await housesService.searchHouses(searchQuery)
+
+            response.send(houses)
+
+
+        }
+
+        catch (error) {
+            next(error)
+
+
+        }
+
+
+
     }
 
     async getAllHouses(request, response, next) {
--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -3,6 +3,21 @@ import { dbContext } from "../db/DbContext.js"
 
 class HousesService {
 
+
+    async searchHouses(searchQuery) {
+
+        const houses = await dbContext.Houses.find(searchQuery)
+
+        return houses
+
+
+
+    }
+
+    // Yes, I can manually enter the specifics in the find like the lab does, but much easier to just put SearchQuery in the find, and then I can filter however I want in postman.
+
+
+
     async getAllHouses() {
 
         const houses = await dbContext.Houses.find()
